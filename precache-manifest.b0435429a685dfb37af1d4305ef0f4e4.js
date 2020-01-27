51059c6d7d095a259ebbb825fd23b28df3397d7b
rj <rraj411@gmail.com>
2020-01-27 21:20:59 +0530

    Updates

---
+++ precache-manifest.b0435429a685dfb37af1d4305ef0f4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88a304e5dd23e205a7fa24f35037218",
+    "url": "/forbidex/index.html"
+  },
+  {
+    "revision": "3bdd7474f096abc20109",
+    "url": "/forbidex/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e6a2a4ecfb036fb9ed7",
+    "url": "/forbidex/static/js/2.0c68d8fa.chunk.js"
+  },
+  {
+    "revision": "fbcf3c448a539323d6bc73351e394b8f",
+    "url": "/forbidex/static/js/2.0c68d8fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bdd7474f096abc20109",
+    "url": "/forbidex/static/js/main.a52541c9.chunk.js"
+  },
+  {
+    "revision": "b782b8b6ecd477c908ab",
+    "url": "/forbidex/static/js/runtime-main.adca60f6.js"
+  }
+]);

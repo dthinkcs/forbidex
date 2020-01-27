9840ca6a5b49300d0ae8c04403ade213ce49e8d0
rj <rraj411@gmail.com>
2020-01-27 21:17:02 +0530

    add files for ghub pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const forbes = require('forbes-list')
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      people: [
+        {
+          personName: "Jeff Bezos",
+          rank: 1
+        }, 
+        {
+          personName: "Bill Gates",
+          rank: 2
+        }, 
+        {
+          personName: "Warren Buffet", 
+          rank: 3
+        }
+      ]
+    }
+
+  }
+
+  componentDidMount() {
+ 
+    forbes.list({ limit: 10 }).then(response => {
+      this.setState({ people: response })
+    })
+  }
+
+  // only needs to change a part of the dom instead of the entire thing
+  render() {
+    return (
+      <div className="App">
+        {this.state.people.map(person => <h1 key={person.rank}> {person.personName} </h1>)}
+      </div>
+    );
+  }
 }
 
 export default App;
